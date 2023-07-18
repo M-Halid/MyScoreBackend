16a0ea44d2aa31c34065f4b501e1e431d357c361
M-Halid <59921239+M-Halid@users.noreply.github.com>
2023-07-19 01:56:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const cors = require("cors")
+const monk = require("monk")
+const app = express()
+
+
+const db = monk('mongodb+srv://Halid:4534Mongo.@cluster0.7e6me.mongodb.net/')
+const score = db.get("score")
+
+
+app.enable('trust proxy');
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Miyaw hi haloo asd heyyy🐈"
+    })
+})
+
+
+
+app.get("/score", (req, res) => {
+    score
+        .find()
+        .then(score => {
+            res.json(score)
+        })
+})
+
+
+app.post("/score", (req, res) => {
+
+
+    const data = {
+        PlayerName: req.body.playerName.toString(),
+        GameScore: req.body.score.toString(),
+        Created: new Date()
+    }
+
+    score
+        .insert(data)
+        .then(createdScore => {
+            res.json(createdScore)
+        })
+
+
+
+}
+)
+
+let PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+
+
+
