a42f598f9c90369142b3b537c5c206b36060a26c
M-Halid <59921239+M-Halid@users.noreply.github.com>
2023-07-19 05:11:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import express, { json } from "express"
 import cors from "cors"
 import monk from "monk"
@@ -6,54 +5,83 @@ const app = express()
 
 
 const db = monk('mongodb+srv://Halid:4534Mongo.@cluster0.7e6me.mongodb.net/')
+db.then(() => {
+    console.log("connected")
+}
+).catch((err) => {
+    console.log("database connection failed")
+    console.log(err)
+})
+
 const score = db.get("score")
-console.log(score)
+
 
 app.enable('trust proxy');
 
 app.use(cors())
 app.use(json())
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Miyaw hi haloo asd heyyy🐈"
-  })
-})
-
-
 
 app.get("/score", (req, res) => {
-  score
-    .find()
-    .then(score => {
-      res.json(score)
+
+    res.json({
+        message: "Miyaw hi haloo asd heyyy🐈"
     })
 })
 
 
-app.post("/score", (req, res) => {
+
+app.get("/", (req, res, next) => {
+    console.log("HI!")
+
+    score
+        .find({})
+        .then(score => {
+            console.log("HI wieder!")
+            res.json(score)
+            console.table(score)
+        }).catch((err) => {
+            console.log("Fehler:" + err)
+            next()
+        });
 
 
-  const data = {
-    PlayerName: req.body.playerName.toString(),
-    GameScore: req.body.score.toString(),
-    Created: new Date()
-  }
 
-  score
-    .insert(data)
-    .then(createdScore => {
-      res.json(createdScore)
-    })
+})
+
+
+const createScore = (req, res, next) => {
+
+
+    const data = {
+        PlayerName: req.body.playerName.toString(),
+        GameScore: req.body.score.toString(),
+        Created: new Date()
+    }
+
+    score
+        .insert(data)
+        .then(createdScore => {
+            res.json(createdScore)
+        }).catch(next);
 
 
 
 }
-)
-
-let PORT = process.env.PORT;
-
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
 
 
+app.post('/', createScore)
 
+app.use((error, req, res, next) => {
+    res.status(500);
+    res.json({
+        message: error.message
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+
+const server = app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+
+server.timeout = 1000
