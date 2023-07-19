00124321a9d6849dc8832bd2895f9add9bf50da5
M-Halid <59921239+M-Halid@users.noreply.github.com>
2023-07-19 04:42:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
-const express = require("express")
-const cors = require("cors")
-const monk = require("monk")
+
+import express, { json } from "express"
+import cors from "cors"
+import monk from "monk"
 const app = express()
 
 
 const db = monk('mongodb+srv://Halid:4534Mongo.@cluster0.7e6me.mongodb.net/')
 const score = db.get("score")
-
+console.log(score)
 
 app.enable('trust proxy');
 
 app.use(cors())
-app.use(express.json())
+app.use(json())
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "Miyaw hi haloo asd heyyy🐈"
-    })
+  res.json({
+    message: "Miyaw hi haloo asd heyyy🐈"
+  })
 })
 
 
 
 app.get("/score", (req, res) => {
-    score
-        .find()
-        .then(score => {
-            res.json(score)
-        })
+  score
+    .find()
+    .then(score => {
+      res.json(score)
+    })
 })
 
 
 app.post("/score", (req, res) => {
 
 
-    const data = {
-        PlayerName: req.body.playerName.toString(),
-        GameScore: req.body.score.toString(),
-        Created: new Date()
-    }
+  const data = {
+    PlayerName: req.body.playerName.toString(),
+    GameScore: req.body.score.toString(),
+    Created: new Date()
+  }
 
-    score
-        .insert(data)
-        .then(createdScore => {
-            res.json(createdScore)
-        })
+  score
+    .insert(data)
+    .then(createdScore => {
+      res.json(createdScore)
+    })
 
 
 
