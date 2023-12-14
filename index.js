b17fe07268bd7caefff75a1f2c1fed5c5f46708a
Muhammed Halid Kutsal <mhalidkutsal@gmail.con>
2023-12-14 14:51:12 +0100

    get the data from mongo db

--- index.js
+++ index.js
@@ -1,75 +1,92 @@
+{/*import express, { json } from "express"
+import cors from "cors"
+import monk from "monk"
+*/}
+
 const express = require("express");
 const cors = require("cors");
 const monk = require("monk");
 const { json } = require("express");
 
-const app = express();
-
-// Connect to MongoDB
-const db = monk('mongodb+srv://Halid:4534Mongo.@cluster0.7e6me.mongodb.net/');
+const app = express()
+const db = monk('mongodb+srv://Halid:4534Mongo.@cluster0.7e6me.mongodb.net/score')
 db.then(() => {
-    console.log("Connected to the database");
-}).catch((err) => {
-    console.error("Database connection failed");
-    console.error(err);
-});
+    console.log("connected")
+}
+).catch((err) => {
+    console.log("database connection failed")
+    console.log(err)
+})
 
-// Access the "score" collection
-const score = db.get("score");
+const score = db.get("score")
+
 
 app.enable('trust proxy');
 
-// Middleware
-app.use(cors());
-app.use(json());
+app.use(cors())
+app.use(json())
 
-// Route to get a sample score
+
 app.get("/score", (req, res) => {
+
     res.json({
         message: "Miyaw hi haloo asd heyyy🐈"
-    });
-});
+    })
+})
 
-// Route to get all scores
-app.get("/", async (req, res, next) => {
-    try {
-        const scores = await score.find({});
-        res.json(scores);
-        console.table(scores);
-    } catch (err) {
-        console.error("Error:", err);
-        next(err);
-    }
-});
 
-// Middleware to create a new score
-const createScore = async (req, res, next) => {
+
+app.get("/", (req, res, next) => {
+    console.log("HI!")
+
+    score
+        .find({})
+        .then(score => {
+            console.log("HI wieder!")
+            res.json(score)
+            console.table(score)
+        }).catch((err) => {
+            console.log("Fehler:" + err)
+            next()
+        });
+
+
+
+})
+
+
+const createScore = (req, res, next) => {
+
+
     const data = {
         PlayerName: req.body.playerName.toString(),
         GameScore: req.body.score.toString(),
         Created: new Date()
-    };
-
-    try {
-        const createdScore = await score.insert(data);
-        res.json(createdScore);
-    } catch (err) {
-        next(err);
     }
-};
 
-// Route to create a new score
-app.post('/', createScore);
+    score
+        .insert(data)
+        .then(createdScore => {
+            res.json(createdScore)
+        }).catch(next);
 
-// Error handling middleware
+
+
+}
+
+
+app.post('/', createScore)
+
 app.use((error, req, res, next) => {
-    res.status(500).json({
+    res.status(500);
+    res.json({
         message: error.message
     });
 });
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
-server.timeout = 1000;
+const server = app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+
+server.timeout = 1000
